b8e737b9a9ea6695e93183670253667fae49144a
prothegee <prothegee@outlook.com>
2023-07-26 16:52:06 +0700

    utility base class

---
+++ src/prthgts/classes/Utility.ts
@@ -0,0 +1,14 @@
+export namespace prthgts
+{
+    /**
+     * @brief prthgts utility class
+    */
+    export class CUtility
+    {
+        public readonly m_alphanumeric:string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        constructor()
+        {
+        }
+    }
+}
