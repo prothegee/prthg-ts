8fac4c78ba133615042e791df4e06b2407f179a0
prothegee <prothegee@outlook.com>
2023-07-31 11:41:46 +0700

    accumulative update 0.0.1.20230731

--- test/main_test.ts
+++ test/main_test.ts
@@ -1,59 +1,52 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import prthgts from "../src/prthgts/prthgts";
+
+
+async function RunTestUtilityClass()
+{
+    const util = prthgts.util;
+
+    console.log("Generate Random Alphanumeric:");
+    for (let i = 0; i < 10; i++)
+    {
+        console.log(`${util.GenerateRandomAlphanumeric(i)}`);
+    }
+
+    console.log("\nGenerate Random UUID:");
+    for (let i = 0; i < 10; i++)
+    {
+        const uuid = util.GenerateRandomUUID();
+        console.log(`${uuid}\n${util.ChangeInputLetterCase(uuid, 0)}\n${util.ChangeInputLetterCase(uuid, 1)}\n${util.ChangeInputLetterCase(uuid, 2)}\n`);
+    }
+
+    const t1 = "wqeasd";
+    if (util.CheckInputIsAlphabetic(t1))
+    {
+        console.log(`${t1} is alphabetic\n`);
+    }
+
+    const t2 = "asdzxc123456";
+    if (util.CheckInputIsAlphaNumeric(t2))
+    {
+        console.log(`${t2} is alphanumeric\n`);
+    }
+}
+
+
+// async function RunTestCryptographyClass()
+// {
+// }
 
 
 async function main()
 {
-    let errCount = 0;
+    console.log("Test CUtilty: Started\n");
+    await RunTestUtilityClass();
+    console.log("Test CUtilty: Finished\n");
 
-    prthgts.log.DEBUG("start test: prthts");
-    console.log("-----------------------------------------------");
-    console.log("===============================================");
-
-    const util = prthgts.utils;
-
-    console.log("\n# generating alphanumer");
-    for (let i = 0; i < 10; i++)
-    {
-        console.log(`${i+1}:\n${util.GenRandomAlphanumeric(i)}\n`);
-    }
-
-    console.log("-----------------------------------------------");
-    console.log("\n# generating uuid");
-    for (let i = 0; i < 10; i++)
-    {
-        const uuid = util.GenRandomUUID();
-        console.log(`${i+1}:\n${uuid}\n${util.ChangeInputLetterCase(uuid, 0)}\n${util.ChangeInputLetterCase(uuid, 1)}\n${util.ChangeInputLetterCase(uuid, 2)}\n`);
-    }
-
-    console.log("-----------------------------------------------");
-    const t1 = "wqeasd";
-    if (util.CheckInputIsAlphabetic(t1))
-    {
-        console.log(`#t1: ${t1} | is alphabetic\n`);
-    }
-    else
-    {
-        console.error(`#t1 error\n`);
-        errCount += 1;
-    }
-
-    console.log("-----------------------------------------------");
-    const t2 = "asdzxc123456";
-    if (util.CheckInputIsAlphaNumeric(t2))
-    {
-        console.log(`#t2: ${t2} | is alphanumeric\n`);
-    }
-    else
-    {
-        console.error(`#t2 error\n`);
-        errCount += 1;
-    }
-
-    console.log("===============================================");
-    console.log("-----------------------------------------------");
-    prthgts.log.DEBUG("end test: prthts");
-    prthgts.log.DEBUG(`error: ${errCount}`);
+    
+    // console.log("Test CCryptography: Started\n");
+    // RunTestCryptographyClass();
+    // console.log("Test CCryptography: Finished\n");
 }
 
 
